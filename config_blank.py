b82b4ba458bc42eebb4abe7c1e8e712d51df36e3
David Surber <dsurber@wisc.edu>
2020-01-21 16:02:54 -0600

    config_blank.py added for new users to insert local settings for their own config.py

---
+++ config_blank.py
@@ -0,0 +1,25 @@
+### System specific api addresses and access tokens
+
+# Grant Numbers to Track in PubMed 
+grant_list = ['']
+
+# Start Date of reporting period 
+start = "04/01/2017"
+# End Date of reporting period 
+end = "mm/dd/YYYY"
+
+# REDCap API uri 
+rc_uri = ""
+# REDCap API Token for Publication Tracking project
+rc_token = ""
+
+# MyNCBI Login 
+ncbi_login = ""
+# MyNCBI Password
+ncbi_pass = ""
+
+# NCBI API Key 
+ncbi_api = ""
+
+# URL for NIH table of Method A Publishers 
+method_a = "http://www.ncbi.nlm.nih.gov/pmc/front-page/NIH_PA_journal_list.csv"
