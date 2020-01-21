49ed8cf193c14068fc6f1a210f3980b6533a434a
dsurber <davidasurber@gmail.com>
2020-01-21 15:20:29 -0600

    trying to get .gitignore to work on config.py

--- config.py
+++ config.py
@@ -1,25 +1,25 @@
 ### System specific api addresses and access tokens
 
-# Grant Numbers to Track in PubMed 
+# Grant Numbers to Track in PubMed
 grant_list = ['UL1RR025011', 'KL2RR025012', 'TL1RR025013', 'UL1TR000427', 'TL1TR000429', 'KL2TR000428', 'U54TR000021', 'UL1TR002373', 'KL2TR002374', 'TL1TR002375']
 
-# Start Date of reporting period 
+# Start Date of reporting period
 start = "04/01/2017"
-# End Date of reporting period 
+# End Date of reporting period
 end = "mm/dd/YYYY"
 
-# REDCap API uri 
+# REDCap API uri
 rc_uri = "https://redcap.ictr.wisc.edu/api/"
 # REDCap API Token for Publication Tracking project
-rc_token = "F174B46CCF8B562DB25507CC5B229DD8"
+rc_token = ""
 
-# MyNCBI Login 
+# MyNCBI Login
 ncbi_login = "ictreval"
 # MyNCBI Password
 ncbi_pass = ""
 
-# NCBI API Key 
+# NCBI API Key
 ncbi_api = "66b67c02832e95b3d85135ead126e8f50808"
 
-# URL for NIH table of Method A Publishers 
+# URL for NIH table of Method A Publishers
 method_a = "http://www.ncbi.nlm.nih.gov/pmc/front-page/NIH_PA_journal_list.csv"
